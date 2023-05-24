6199744df76f2d107cec9b1802b8b955323c8420
Расул Алиев <r.aliev@jetmail.cc>
2023-05-24 12:51:18 +0300

    init project

---
+++ src/App.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import './App.css';
+import useSpeechToText, { ResultType } from 'Hooks';
+import micIcon from './mic.svg';
+
+function App() {
+  const {
+    error,
+    interimResult,
+    isRecording,
+    results,
+    startSpeechToText,
+    stopSpeechToText
+  } = useSpeechToText({
+    continuous: true,
+    crossBrowser: false,
+    googleApiKey: process.env.REACT_APP_API_KEY,
+    speechRecognitionProperties: { interimResults: true, lang: 'en-US' },
+    useLegacyResults: false
+  });
+
+  if (error) {
+    return (
+      <div
+        style={{
+          maxWidth: '600px',
+          margin: '100px auto',
+          textAlign: 'center'
+        }}
+      >
+        <p>
+          {error}
+          <span style={{ fontSize: '3rem' }}>🤷‍</span>
+        </p>
+      </div>
+    );
+  }
+
+  return (
+    <div
+      style={{
+        maxWidth: '600px',
+        margin: '100px auto',
+        textAlign: 'center'
+      }}
+    >
+      <h1>Recording: {isRecording.toString()}</h1>
+      <button onClick={isRecording ? stopSpeechToText : startSpeechToText}>
+        <span>{isRecording ? 'Stop Recording' : 'Start Recording'}</span>
+        <img data-recording={isRecording} src={micIcon} alt="" />
+      </button>
+      <ul>
+        {(results as ResultType[]).map((result) => (
+          <li key={result.timestamp}>{result.transcript}</li>
+        ))}
+        {interimResult && <li>{interimResult}</li>}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
